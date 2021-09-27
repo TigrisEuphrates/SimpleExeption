8ae294e4392c352b2ff56d9eca579a65652b54ac
Tigris <tabula.tenebris@gmail.com>
2021-09-27 21:25:58 +0600

    sdadasdsd

---
+++ SimpleExeption/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleExeption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                    myCar.Accelerate(10);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ERROR:");
+                Console.WriteLine($"Method: {e.TargetSite}");
+
+                Console.WriteLine($"Class defining member: {e.TargetSite.DeclaringType}");
+                Console.WriteLine($"Member type: {e.TargetSite.MemberType}");
+                Console.WriteLine($"Stack: {e.StackTrace}");
+                Console.WriteLine($"Help link: {e.HelpLink}");
+                //Console.WriteLine($"Time: {e.Data}");
+                Console.WriteLine("Custom Data:");
+                foreach (DictionaryEntry item in e.Data)
+                {
+                    Console.WriteLine($"{item.Key} : {item.Value}");
+                }
+
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Source: {e.Source}");
+            }
+            Console.WriteLine("\nOut of exeption logic.");
+            Console.WriteLine();
+        }
+    }
+}
